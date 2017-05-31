2a358347ba86b488c5829436fcc8e6c7e81a0ed3
Sudharsan Prabu <prabus@cs.washington.edu>
2017-05-30 23:15:44 -0700

    this is the ICMP version

--- proto/proto.go
+++ proto/proto.go
@@ -1,10 +1,8 @@
 package proto
 
 import (
-    "fmt"
-	"net"
-	"time"
-	"github.com/tatsushid/go-fastping"
+    "github.com/sparrc/go-ping"
+    "time"
 )
 
 type Client_Init_Packet struct {
@@ -23,27 +21,13 @@ type Mp3_Frame_Packet struct {
 
 // get RTT in terms of milliseconds between current node and specified IP
 func GetRTTBetweenNodes(address string) int64 {
-    pinger := fastping.NewPinger()
+    pinger, err := ping.NewPinger(address[0:9])
+    if err != nil {
+        panic(err)
+    }
 
-	_, err := pinger.Network("udp")
-	// We shouldn't ever get an error but we're checking anyway
-	if err != nil {
-		panic("Error setting network type: " + err.Error())
-	}
-
-	addr, err := net.ResolveIPAddr("ip", address)
-	if err != nil {
-		panic("Error resolving IP Address: " + err.Error())
-	}
-
-	pinger.AddIPAddr(addr)
-	pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-		fmt.Printf("%s time=%v seconds\n", addr, rtt.Seconds())
-	}
-
-	if err = pinger.Run(); err != nil {
-		panic(err)
-	}
-
-    return 0
+    pinger.Count = 1
+    pinger.Run()
+    stats := pinger.Statistics()
+    return int64(stats.MinRtt / time.Millisecond)
 }
